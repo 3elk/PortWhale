0fa9238b0dd562d9a6d9ae9232c7fa1e87acf5e2
elk <194042783+3elk@users.noreply.github.com>
2025-03-24 01:08:57 +0000

    Update Program.cs

--- PortWhale/src/Program.cs
+++ PortWhale/src/Program.cs
@@ -1,63 +1,75 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using System.Linq;
 
-namespace MainSpace
+class PortScanner
 {
-    class PortScanner
+    static void Main(string[] args)
     {
-        public async Task ScanAsync(string ipAddress, int startPort, int endPort)
-        {
-            var openPorts = new ConcurrentBag<int>();
-            var tasks = new List<Task>();
 
-            for (int port = startPort; port <= endPort; port++)
-            {
-                tasks.Add(ScanPortAsync(ipAddress, port, openPorts));
-            }
-
-            await Task.WhenAll(tasks);
-            Console.WriteLine("SCAN COMPLETED! | 🚀");
-        }
-
-        private async Task ScanPortAsync(string ipAddress, int port, ConcurrentBag<int> openPorts)
-        {
-            using (TcpClient tcpClient = new TcpClient())
-            {
-                try
-                {
-                    var connectTask = tcpClient.ConnectAsync(ipAddress, port);
-                    if (await Task.WhenAny(connectTask, Task.Delay(200)) == connectTask)
-                    {
-                        openPorts.Add(port);
-                        Console.WriteLine($"PORT {port} is OPEN | ✅");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"PORT {port} is CLOSED | ❌");
-                    }
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine($"PORT {port} is CLOSED | ❌");
-                }
-            }
-        }
-
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine(@"
-   ___           _   __    __ _           _                  . 
+        Console.Title = "PortWhale - Port Scanner";
+        Console.WriteLine(@"
+   ___           _   __    __ _           _                 \'/ 
   / _ \___  _ __| |_/ / /\ \ \ |__   __ _| | ___          ___:____     |""\/""
  / /_)/ _ \| '__| __\ \/  \/ / '_ \ / _` | |/ _ \       ,'        `.    \  /
 / ___/ (_) | |  | |_ \  /\  /| | | | (_| | |  __/       |  O        \___/  |
 \/    \___/|_|   \__| \/  \/ |_| |_|\__,_|_|\___|~^~^~^~^~^~^~^~^~^~^~^~^~~^~^~^~
 
-                            The Ultimate Port Scanner
-                            By https://github.com/3elk  
-            ");
-            Console.Write("Enter IP Address >> "); string ipAddress = Console.ReadLine(); Console.Write("Enter Start Port >> "); int startPort = int.Parse(Console.ReadLine()); Console.Write("Enter End Port >> "); int endPort = int.Parse(Console.ReadLine()); PortScanner scanner = new PortScanner(); await scanner.ScanAsync(ipAddress, startPort, endPort);
+                            Simple Port Scanner
+                        BY: https://3elk.github.io
+
+");
+        Console.Write("Enter IP Address >> ");
+        string ipAddress = Console.ReadLine();
+        Console.Write("Enter start port >> ");
+        int startPort = int.Parse(Console.ReadLine());
+        Console.Write("Enter end port >> ");
+        int endPort = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n~^~^~^~^~^~^STARTING SCAN~^~^~^~^~^~^\n");
+        var openPorts = new ConcurrentBag<int>();
+        Parallel.For(startPort, endPort + 1, port =>
+        {
+            ScanPort(ipAddress, port, openPorts);
+        });
+
+        var sortedPorts = openPorts.OrderBy(p => p).ToList();
+        Console.WriteLine($"\n^~^~^~^~SCAN COMPLETED~^~^~^~^\n");
+        Console.WriteLine($"!FOUND {sortedPorts.Count} OPEN PORTS!\n");
+
+        if (sortedPorts.Any())
+        {
+            Console.WriteLine("OPEN PORTS >>\n");
+            foreach (var port in sortedPorts)
+            {
+                Console.WriteLine($"PORT {port} | ✓ OPEN");
+            }
+        }
+
+        Console.WriteLine("\nPress Enter to exit . . .");
+        Console.ReadKey();
+    }
+
+    static void ScanPort(string ipAddress, int port, ConcurrentBag<int> openPorts)
+    {
+        using (TcpClient tcpClient = new TcpClient())
+        {
+            try
+            {
+                tcpClient.Connect(ipAddress, port);
+                openPorts.Add(port);
+                Console.WriteLine($"PORT {port} | ✓");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine($"PORT {port} | ✕");
+            }
+            finally
+            {
+                tcpClient.Close();
+            }
         }
     }
 }
